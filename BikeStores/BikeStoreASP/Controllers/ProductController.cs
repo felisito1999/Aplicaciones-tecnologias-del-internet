0dea86c60f1239c845896ad2ed4ac2eba07196aa
felisito1999 <felejunier@hotmail.com>
2020-06-11 12:10:56 -0400

    Carpeta

---
+++ BikeStores/BikeStoreASP/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BikeDataLibrary.Services;
+
+namespace BikeStoreASP.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult AllProducts()
+        {
+            var products = GetService.GetProductService().GetAll();
+            var productsViewModel = GetService.
+
+            return View(products);
+        }
+    }
+}
---
+++ BikeStores/BikeStoreASP/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BikeStoreASP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BikeStoreASP")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("51abe0ae-23fe-4d67-8fe4-97633f232770")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
